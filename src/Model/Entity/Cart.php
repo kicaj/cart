2600eb13e5478985e813ae946349b4fc742e1501
kicaj <kicaj@kdev.pl>
2019-12-01 23:13:33 +0100

    Add two methods of return statuses

--- src/Model/Entity/Cart.php
+++ src/Model/Entity/Cart.php
@@ -15,4 +15,34 @@ class Cart extends Entity
     public const CART_STATUS_NEW = 1;
     public const CART_STATUS_PENDING = 2;
     public const CART_STATUS_COMPLET = 3;
+
+    /**
+     * List of statuses.
+     *
+     * @return array List of statuses.
+     */
+    public static function getStatuses()
+    {
+        return [
+            self::CART_STATUS_MERGED => __d('cart', 'Merged'),
+            self::CART_STATUS_REJECT => __d('cart', 'Rejected'),
+            self::CART_STATUS_OPEN => __d('cart', 'Open'),
+            self::CART_STATUS_NEW => __d('cart', 'New'),
+            self::CART_STATUS_PENDING => __d('cart', 'Pending'),
+            self::CART_STATUS_COMPLET => __d('cart', 'Completed'),
+        ];
+    }
+
+    /**
+     * Get status.
+     *
+     * @param integer $status Status identifier.
+     * @return string Status.
+     */
+    public static function getStatus($status)
+    {
+        if (array_key_exists($status, self::getStatuses())) {
+            return self::getStatuses()[$status];
+        }
+    }
 }
