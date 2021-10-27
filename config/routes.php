87d0e2092a86211f5d75db7d4fcef717a2e33c6e
kicaj <kicaj@kdev.pl>
2021-10-27 22:56:03 +0200

    Update routes.php

--- config/routes.php
+++ config/routes.php
@@ -1,16 +1,17 @@
 <?php
-use Cake\Routing\RouteBuilder;
-use Cake\Routing\Router;
 use Cake\Routing\Route\DashedRoute;
+use Cake\Routing\RouteBuilder;
 
-Router::prefix('admin', function (RouteBuilder $routes) {
-    $routes->plugin('Cart', function (RouteBuilder $routes) {
+return static function (RouteBuilder $routes) {
+    $routes->prefix('admin', function (RouteBuilder $routes) {
+        $routes->plugin('Cart', function (RouteBuilder $routes) {
+            $routes->fallbacks(DashedRoute::class);
+        });
+
         $routes->fallbacks(DashedRoute::class);
     });
 
-    $routes->fallbacks(DashedRoute::class);
-});
-
-Router::plugin('Cart', function (RouteBuilder $routes) {
-    $routes->fallbacks(DashedRoute::class);
-});
+    $routes->plugin('Cart', function (RouteBuilder $routes) {
+        $routes->fallbacks(DashedRoute::class);
+    });
+};
